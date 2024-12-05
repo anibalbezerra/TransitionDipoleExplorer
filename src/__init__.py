d80f646fefaee170b2576f26effdfc499e3fc1dd
ANIBAL THIAGO BEZERRA <85360329+anibalbezerra@users.noreply.github.com>
2024-12-05 13:26:07 -0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .projectionAnalyser import analyzer # Assuming ProjectionAnalyser is the class name
+from .coloredLogger import color_log
+from .openBin import parse_eps_binaries as parseEps
+from .projParser import projWFC_parser as parseProj
