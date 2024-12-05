b2bdbc5f0b7d5ceccd53d5a5f2bbfc96eb24589d
ANIBAL THIAGO BEZERRA <85360329+anibalbezerra@users.noreply.github.com>
2024-12-05 11:24:48 -0300

    Update coloredLogger.py

--- src/coloredLogger.py
+++ src/coloredLogger.py
@@ -14,38 +14,69 @@ def trace(self, message, *args, **kwargs):
 logging.Logger.trace = trace
 
 class color_log:
+    """
+    A class to handle colored logging with a custom TRACE level.
+    """
     
     def __init__(self, level) -> None:
+        """
+        Initialize the color_log class with a specified log level.
+
+        :param level: The log level to set ('info', 'debug', 'trace').
+        :type level: str
+        """
         self.logger = self.setup_logger(level)
         
-
     def setup_logger(self, level):
+        """
+        Set up the logger with the specified log level and colored formatting.
+
+        :param level: The log level to set ('info', 'debug', 'trace').
+        :type level: str
+        :return: The configured logger instance.
+        :rtype: logging.Logger
+        """
         # Create a log formatter with color
         log_format = ('%(log_color)s%(levelname)s: %(message)s')        
-        color_formatter = colorlog.ColoredFormatter(# Define the color mapping for each log level
-            log_format, log_colors={'DEBUG': 'cyan', 'INFO': 'green','WARNING': 'yellow','ERROR': 'red', 'CRITICAL': 'bold_red','TRACE': 'blue', } )    
-        logger = logging.getLogger() # Create a logger instance    
+        color_formatter = colorlog.ColoredFormatter(
+            log_format, 
+            log_colors={
+                'DEBUG': 'cyan', 
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red', 
+                'CRITICAL': 'bold_red',
+                'TRACE': 'blue', 
+            }
+        )    
+        logger = logging.getLogger()  # Create a logger instance    
 
-        # Create a logger instance
-        logger = logging.getLogger()
-
+        # Set the minimum log level based on the input
         if level == 'info':
-            logger.setLevel(logging.INFO) # Set the minimum log level (DEBUG, INFO, WARNING, etc.) 
+            logger.setLevel(logging.INFO)  # Set the minimum log level to INFO
         elif level == 'debug':  
-            logger.setLevel(logging.DEBUG) # Set the minimum log level (DEBUG, INFO, WARNING, etc.)
+            logger.setLevel(logging.DEBUG)  # Set the minimum log level to DEBUG
         elif level == 'trace':
-            logger.setLevel(TRACE_LEVEL_NUM)  # Custom TRACE level
+            logger.setLevel(TRACE_LEVEL_NUM)  # Set the minimum log level to custom TRACE level
         
-        console_handler = logging.StreamHandler() # Create a console handler and set its log level
+        console_handler = logging.StreamHandler()  # Create a console handler and set its log level
         console_handler.setLevel(logging.DEBUG)        
-        console_handler.setFormatter(color_formatter)# Set the formatter to the console handler    
-        # Add the console handler to the logger
+        console_handler.setFormatter(color_formatter)  # Set the formatter to the console handler    
+        
+        # Add the console handler to the logger if it doesn't already have handlers
         if not logger.hasHandlers():
             logger.addHandler(console_handler)
+        
         return logger
 
-    # Define a custom trace method
     def trace(self, message, *args, **kwargs):
+        """
+        Log a message with the TRACE level.
+
+        :param message: The message to log.
+        :type message: str
+        :param args: Additional arguments.
+        :param kwargs: Additional keyword arguments.
+        """
         if self.logger.isEnabledFor(TRACE_LEVEL_NUM):
             self.logger.log(TRACE_LEVEL_NUM, message, *args, **kwargs)
-
