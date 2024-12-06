2fd40b026c26b395f2ae2d760c38a5fabdd57829
anibalbezerra <anibal.bezerra@gmail.com>
2024-12-06 09:51:12 -0300

    update

--- setup.py
+++ setup.py
@@ -5,17 +5,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy'
-        'scipy'
-        'matplotlib'
-        'pandas'
-        'multiprocessing'
-        'os'
-        'logging'
-        'colorlog'
-        're'
-        'math'
-        'time'
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'colorlog',
     ],
     entry_points={
         'console_scripts': [
